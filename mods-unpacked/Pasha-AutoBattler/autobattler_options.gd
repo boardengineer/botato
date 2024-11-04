cea8182430ea38cf8acca0a86de59d0cfca20212
boardengineer <77598527+boardengineer@users.noreply.github.com>
2024-11-04 13:17:14 -0800

    Save configs to file

--- mods-unpacked/Pasha-AutoBattler/autobattler_options.gd
+++ mods-unpacked/Pasha-AutoBattler/autobattler_options.gd
@@ -3,94 +3,184 @@ extends Node
 signal setting_changed(setting_name, value, mod_name)
 
 # This Mirrors Configs to act as a backup and make reads shorter without string lookups
-var enable_autobattler
-var enable_ai_visuals
-var enable_ai_marker
-var enable_smoothing
-var smoothing_speed
-var item_weight
-var projectile_weight
-var tree_weight
-var boss_weight
-var bumper_weight
-var egg_weight
-var bumper_distance
+var enable_autobattler : bool = false
+const ENABLE_AUTOBATTLER_OPTION_NAME = "ENABLE_AUTOBATTLER"
 
-var was_space_pressed = false
-var was_shift_pressed = false
+var enable_ai_visuals : bool = false
+const ENABLE_AI_VISUALS_OPTION_NAME = "ENABLE_AI_VISUALS"
+
+var enable_ai_marker : bool = true
+const ENABLE_AI_MARKER_OPTION_NAME = "ENABLE_AI_MARKER"
+
+var enable_smoothing : bool = true
+const ENABLE_SMOOTHING_OPTION_NAME = "ENABLE_SMOOTHING"
+
+var smoothing_speed : float = 1
+const SMOOTHING_SPEED_OPTION_NAME = "SMOOTHING_SPEED"
+
+var item_weight : float = .5
+const ITEM_WEIGHT_OPTION_NAME = "ITEM_WEIGHT"
+
+var projectile_weight : float = 2
+const PROJECTILE_WEIGHT_OPTION_NAME = "PROJECTILE_WEIGHT"
+
+var tree_weight : float = 2
+const TREE_WEIGHT_OPTION_NAME = "TREE_WEIGHT"
+
+var boss_weight : float = 3
+const BOSS_WEIGHT_OPTION_NAME = "BOSS_WEIGHT"
+
+var bumper_weight : float = 2
+const BUMPER_WEIGHT_OPTION_NAME = "BUMPER_WEIGHT"
+
+var egg_weight : float = 5
+const EGG_WEIGHT_OPTION_NAME = "EGG_WEIGHT"
+
+var bumper_distance : float = 300
+const BUMPER_DISTANCE_OPTION_NAME = "BUMPER_DISTANCE"
 
 const DEFAULT_COOLDOWN = .2
 var option_cooldown = DEFAULT_COOLDOWN
+
+const MOD_NAME = "Pasha-AutoBattler"
+const CONFIG_FILENAME = "user://pasha-botato-options.cfg"
+const CONFIG_SECTION = "options"
+
 
 func _ready():
 	reset_defaults()
 	load_mod_options()
 	
+	if not get_node("/root/ModLoader").has_node("dami-ModOptions"):
+		return
+	
+	var mod_configs_interface = get_node("/root/ModLoader/dami-ModOptions/ModsConfigInterface")
+	
+	if mod_configs_interface:
+		mod_configs_interface.connect("setting_changed", self, "setting_changed")
+
+
 func _input(event):
 	if event is InputEventKey:
 		if event.shift and event.scancode == KEY_SPACE and option_cooldown < 0.0:
 			option_cooldown = DEFAULT_COOLDOWN
 			enable_autobattler = not enable_autobattler
-			emit_signal("setting_changed", "ENABLE_AUTOBATTLER", enable_autobattler, "Pasha-AutoBattler")
 			
 			if $"/root/ModLoader".has_node("dami-ModOptions"):
 				var mod_configs_interface = get_node("/root/ModLoader/dami-ModOptions/ModsConfigInterface")
 				var mod_configs = mod_configs_interface.mod_configs
-			
 				if mod_configs.has("Pasha-AutoBattler"):
 					var config = mod_configs["Pasha-AutoBattler"]
 					config["ENABLE_AUTOBATTLER"] = enable_autobattler
 					mod_configs_interface.on_setting_changed("ENABLE_AUTOBATTLER", enable_autobattler, "Pasha-AutoBattler")
-			
+					
+			emit_signal("setting_changed", ENABLE_AUTOBATTLER_OPTION_NAME, enable_autobattler, "Pasha-AutoBattler")
+
 
 func _process(delta):
 	option_cooldown -= delta
 
-func load_mod_options():
+
+func setting_changed(key:String, value, mod) -> void:
+	if mod != MOD_NAME:
+		return
 	
+	if key == ENABLE_AUTOBATTLER_OPTION_NAME:
+		enable_autobattler = value
+	elif key == ENABLE_AI_VISUALS_OPTION_NAME:
+		enable_ai_visuals = value
+	elif key == ENABLE_AI_MARKER_OPTION_NAME:
+		enable_ai_marker = value
+	elif key == ENABLE_SMOOTHING_OPTION_NAME:
+		enable_smoothing = value
+	elif key == SMOOTHING_SPEED_OPTION_NAME:
+		smoothing_speed = value
+	elif key == ITEM_WEIGHT_OPTION_NAME:
+		item_weight = value
+	elif key == PROJECTILE_WEIGHT_OPTION_NAME:
+		projectile_weight = value
+	elif key == TREE_WEIGHT_OPTION_NAME:
+		tree_weight = value
+	elif key == BOSS_WEIGHT_OPTION_NAME:
+		boss_weight = value
+	elif key == BUMPER_WEIGHT_OPTION_NAME:
+		bumper_weight = value
+	elif key == EGG_WEIGHT_OPTION_NAME:
+		egg_weight = value
+	elif key == BUMPER_DISTANCE_OPTION_NAME:
+		bumper_distance = value
+	else:
+		print_debug("WARNING, UNKNOWN CHANGE ", key)
+	
+	save_configs()
+
+
+func load_mod_options():
 	if not $"/root/ModLoader".has_node("dami-ModOptions"):
 		return
-		
-	var mod_configs = get_node("/root/ModLoader/dami-ModOptions/ModsConfigInterface").mod_configs
+	
+	var mod_configs_interface = get_node("/root/ModLoader/dami-ModOptions/ModsConfigInterface")
+	
+	var config = ConfigFile.new()
+	var err = config.load(CONFIG_FILENAME)
+	
+	if err != OK:
+		return
+	
+	enable_autobattler = config.get_value(CONFIG_SECTION, ENABLE_AUTOBATTLER_OPTION_NAME, false)
+	mod_configs_interface.on_setting_changed(ENABLE_AUTOBATTLER_OPTION_NAME, enable_autobattler, MOD_NAME)
+	
+	enable_ai_visuals  = config.get_value(CONFIG_SECTION, ENABLE_AI_VISUALS_OPTION_NAME, false)
+	mod_configs_interface.on_setting_changed(ENABLE_AI_VISUALS_OPTION_NAME, enable_ai_visuals, MOD_NAME)
+	
+	enable_ai_marker   = config.get_value(CONFIG_SECTION, ENABLE_AI_MARKER_OPTION_NAME, true)
+	mod_configs_interface.on_setting_changed(ENABLE_AI_MARKER_OPTION_NAME, enable_ai_marker, MOD_NAME)
+	
+	enable_smoothing   = config.get_value(CONFIG_SECTION, ENABLE_SMOOTHING_OPTION_NAME, true)
+	mod_configs_interface.on_setting_changed(ENABLE_SMOOTHING_OPTION_NAME, enable_smoothing, MOD_NAME)
+	
+	smoothing_speed    = config.get_value(CONFIG_SECTION, SMOOTHING_SPEED_OPTION_NAME, 1)
+	mod_configs_interface.on_setting_changed(SMOOTHING_SPEED_OPTION_NAME, smoothing_speed, MOD_NAME)
+	
+	item_weight        = config.get_value(CONFIG_SECTION, ITEM_WEIGHT_OPTION_NAME, .5)
+	mod_configs_interface.on_setting_changed(ITEM_WEIGHT_OPTION_NAME, item_weight, MOD_NAME)
+	
+	projectile_weight  = config.get_value(CONFIG_SECTION, PROJECTILE_WEIGHT_OPTION_NAME, 2)
+	mod_configs_interface.on_setting_changed(PROJECTILE_WEIGHT_OPTION_NAME, projectile_weight, MOD_NAME)
+	
+	tree_weight        = config.get_value(CONFIG_SECTION, TREE_WEIGHT_OPTION_NAME, 2)
+	mod_configs_interface.on_setting_changed(TREE_WEIGHT_OPTION_NAME, tree_weight, MOD_NAME)
+	
+	boss_weight        = config.get_value(CONFIG_SECTION, BOSS_WEIGHT_OPTION_NAME, 3)
+	mod_configs_interface.on_setting_changed(BOSS_WEIGHT_OPTION_NAME, boss_weight, MOD_NAME)
+	
+	bumper_weight      = config.get_value(CONFIG_SECTION, BUMPER_WEIGHT_OPTION_NAME, 2)
+	mod_configs_interface.on_setting_changed(BUMPER_WEIGHT_OPTION_NAME, bumper_weight, MOD_NAME)
+	
+	egg_weight         = config.get_value(CONFIG_SECTION, EGG_WEIGHT_OPTION_NAME, 5)
+	mod_configs_interface.on_setting_changed(EGG_WEIGHT_OPTION_NAME, egg_weight, MOD_NAME)
+	
+	bumper_distance    = config.get_value(CONFIG_SECTION, BUMPER_DISTANCE_OPTION_NAME, 300)
+	mod_configs_interface.on_setting_changed(BUMPER_DISTANCE_OPTION_NAME, bumper_distance, MOD_NAME)
 
-	if mod_configs.has("Pasha-AutoBattler"):
-		var config = mod_configs["Pasha-AutoBattler"]
-		
-		if config.has("ENABLE_AUTOBATTLER"):
-			enable_autobattler = config["ENABLE_AUTOBATTLER"]
-		
-		if config.has("ENABLE_AI_VISUALS"):
-			enable_ai_visuals = config["ENABLE_AI_VISUALS"]
-		
-		if config.has("ENABLE_AI_MARKER"):
-			enable_ai_marker = config["ENABLE_AI_MARKER"]
-			
-		if config.has("ENABLE_SMOOTHING"):
-			enable_smoothing = config["ENABLE_SMOOTHING"]
-		
-		if config.has("SMOOTHING_SPEED"):
-			smoothing_speed = config["SMOOTHING_SPEED"]
-		
-		if config.has("ITEM_WEIGHT"):
-			item_weight = config["ITEM_WEIGHT"]
-		
-		if config.has("PROJECTILE_WEIGHT"):
-			projectile_weight = config["PROJECTILE_WEIGHT"]
-		
-		if config.has("TREE_WEIGHT"):
-			tree_weight = config["TREE_WEIGHT"]
-		
-		if config.has("BOSS_WEIGHT"):
-			boss_weight = config["BOSS_WEIGHT"]
-		
-		if config.has("BUMPER_WEIGHT"):
-			bumper_weight = config["BUMPER_WEIGHT"]
-		
-		if config.has("EGG_WEIGHT"):
-			egg_weight = config["EGG_WEIGHT"]
-		
-		if config.has("BUMPER_DISTANCE"):
-			bumper_distance = config["BUMPER_DISTANCE"]
+
+func save_configs() -> void:
+	var config = ConfigFile.new()
+	
+	config.set_value(CONFIG_SECTION, ENABLE_AUTOBATTLER_OPTION_NAME, enable_autobattler)
+	config.set_value(CONFIG_SECTION, ENABLE_AI_VISUALS_OPTION_NAME , enable_ai_visuals)
+	config.set_value(CONFIG_SECTION, ENABLE_AI_MARKER_OPTION_NAME  , enable_ai_marker)
+	config.set_value(CONFIG_SECTION, ENABLE_SMOOTHING_OPTION_NAME  , enable_smoothing)
+	config.set_value(CONFIG_SECTION, SMOOTHING_SPEED_OPTION_NAME   , smoothing_speed)
+	config.set_value(CONFIG_SECTION, ITEM_WEIGHT_OPTION_NAME       , item_weight)
+	config.set_value(CONFIG_SECTION, PROJECTILE_WEIGHT_OPTION_NAME , projectile_weight)
+	config.set_value(CONFIG_SECTION, TREE_WEIGHT_OPTION_NAME       , tree_weight)
+	config.set_value(CONFIG_SECTION, BOSS_WEIGHT_OPTION_NAME       , boss_weight)
+	config.set_value(CONFIG_SECTION, BUMPER_WEIGHT_OPTION_NAME     , bumper_weight)
+	config.set_value(CONFIG_SECTION, EGG_WEIGHT_OPTION_NAME        , egg_weight)
+	config.set_value(CONFIG_SECTION, BUMPER_DISTANCE_OPTION_NAME   , bumper_distance)
+	
+	config.save(CONFIG_FILENAME)
 
 
 func reset_defaults() -> void:
--- mods-unpacked/Pasha-AutoBattler/extensions/entities/units/player/player.gd
+++ mods-unpacked/Pasha-AutoBattler/extensions/entities/units/player/player.gd
@@ -8,25 +8,17 @@ func _ready():
 	ai_icon = ai_icon_scene.instance()
 	add_child(ai_icon)
 	
-	var direct_signal = true
-	
-	if $"/root/ModLoader".has_node("dami-ModOptions"):
-		var mod_configs = get_node("/root/ModLoader/dami-ModOptions/ModsConfigInterface").mod_configs
-		if mod_configs.has("Pasha-AutoBattler"):
-			direct_signal = false
-			ModsConfigInterface = get_node("/root/ModLoader/dami-ModOptions/ModsConfigInterface")
-			ModsConfigInterface.connect("setting_changed", self, "on_setting_changed")
-	
-	if direct_signal:
-		var options_node = $"/root/AutobattlerOptions"
-		options_node.connect("setting_changed", self, "on_setting_changed")
-	
 	check_marker_params()
 	check_smoother_params()
+	
+	var options_node = $"/root/AutobattlerOptions"
+	options_node.connect("setting_changed", self, "on_setting_changed")
+
 
 func on_setting_changed(_setting_name:String, _value, _mod_name):
 	check_marker_params()
 	check_smoother_params()
+
 
 func check_marker_params():
 	var options_node = $"/root/AutobattlerOptions"
@@ -40,7 +32,8 @@ func check_marker_params():
 		ai_icon.show()
 	else:
 		ai_icon.hide()
-	
+
+
 func check_smoother_params():
 	var options_node = $"/root/AutobattlerOptions"
 	
