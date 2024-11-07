734c52bcb65275cfe9104bcc4582175b277789c9
boardengineer <77598527+boardengineer@users.noreply.github.com>
2024-11-06 21:04:31 -0800

    multibot initial checkpoint

--- mods-unpacked/Pasha-AutoBattler/mod_main.gd
+++ mods-unpacked/Pasha-AutoBattler/mod_main.gd
@@ -15,5 +15,13 @@ func _init():
 	ModLoaderMod.install_script_extension(ext_dir + "main.gd")
 	ModLoaderMod.install_script_extension(ext_dir + "entities/units/movement_behaviors/player_movement_behavior.gd")
 	ModLoaderMod.install_script_extension(ext_dir + "entities/units/player/player.gd")
+	
+	ModLoaderMod.install_script_extension(ext_dir + "singletons/coop_service.gd")
+	ModLoaderMod.install_script_extension(ext_dir + "singletons/focus_emulator_signal.gd")
+	
+	ModLoaderMod.install_script_extension(ext_dir + "ui/menus/global/focus_emulator.gd")
 	ModLoaderMod.install_script_extension(ext_dir + "ui/menus/pages/main_menu.gd")
+	ModLoaderMod.install_script_extension(ext_dir + "ui/menus/run/character_selection.gd")
+	ModLoaderMod.install_script_extension(ext_dir + "ui/menus/run/weapon_selection.gd")
+	
 	ModLoaderMod.add_translation(trans_dir + "autobattler_options.en.translation")
