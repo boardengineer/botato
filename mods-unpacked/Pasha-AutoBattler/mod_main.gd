11d250cd2f20d0e794d6279a6c1f0dd94382a2ce
boardengineer <77598527+boardengineer@users.noreply.github.com>
2024-11-20 12:24:29 -0800

    Join Instructions
    Update version number for steam release of new feature

--- mods-unpacked/Pasha-AutoBattler/mod_main.gd
+++ mods-unpacked/Pasha-AutoBattler/mod_main.gd
@@ -22,5 +22,6 @@ func _init():
 	ModLoaderMod.install_script_extension(ext_dir + "ui/menus/global/focus_emulator.gd")
 	ModLoaderMod.install_script_extension(ext_dir + "ui/menus/ingame/upgrades_ui.gd")
 	ModLoaderMod.install_script_extension(ext_dir + "ui/menus/pages/main_menu.gd")
+	ModLoaderMod.install_script_extension(ext_dir + "ui/menus/run/coop_join_panel.gd")
 	
 	ModLoaderMod.add_translation(trans_dir + "autobattler_options.en.translation")
